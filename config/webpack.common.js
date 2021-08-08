921f622d9bec339ca4175c1c93c6f46f13fa6671
izaac <im.izaac.seo@gmail.com>
2021-08-08 12:46:22 -0700

    new config

---
+++ config/webpack.common.js
@@ -0,0 +1,63 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // extract css to files
+const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer') // help tailwindcss to work
+
+module.exports = {
+  // Customize the webpack build process
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
+    }),
+
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+
+  // Determine how modules within the project are treated
+  module: {
+    rules: [
+      // JavaScript: Use Babel to transpile JavaScript files
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ['babel-loader'] },
+
+      // Styles: Inject CSS into the head with source maps
+      {
+        test: /\.(css|scss|sass)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'postcss-loader', // postcss loader needed for tailwindcss
+            options: {
+              postcssOptions: {
+                ident: 'postcss',
+                plugins: [tailwindcss, autoprefixer],
+              },
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+
+      // Images: Copy image files to build folder
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+
+      // Fonts and SVGs: Inline files
+      { test: /\.(woff(2)?|eot|ttf|otf|)$/, type: 'asset/inline' },
+      // TODO: Remove the below rule for .mjs, once aws-amplify supports webpack 5
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+}
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,53 +1,48 @@
-const paths = require('./paths')
-const Dotenv = require('dotenv-webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { ModuleFederationPlugin } = require('webpack').container
+const path = require('path')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = merge(common, {
-  // Set the mode to development or production
-  mode: 'development',
-
-  // Control how source maps are generated
-  devtool: 'inline-source-map',
-
-  // Spin up a server for quick development
-  devServer: {
-    historyApiFallback: true,
-    contentBase: paths.build,
-    open: false,
-    compress: true,
-    hot: true,
-    port: 3000,
+  entry: {
+    main: './src/index',
   },
-
-  module: {
-    rules: [
-      // ... other rules
-      {
-        test: /\.[js]sx?$/,
-        exclude: /node_modules/,
-        use: [
-          // ... other loaders
-          {
-            loader: require.resolve('babel-loader'),
-            options: {
-              // ... other options
-              plugins: [
-                // ... other plugins
-                require.resolve('react-refresh/babel'),
-              ].filter(Boolean),
-            },
-          },
-        ],
-      },
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3002,
+  },
+  output: {
+    publicPath: 'auto',
+    clean: true,
+  },
+  resolve: {
+    extensions: [
+      '.jsx',
+      '.js',
+      '.json',
+      '.css',
+      '.scss',
+      '.jpg',
+      'jpeg',
+      'png',
     ],
   },
   plugins: [
-    new Dotenv({
-      path: './.env.development',
+    new ModuleFederationPlugin({
+      name: 'auth-app',
+      library: { type: 'var', name: 'auth_app' },
+      filename: 'remoteEntry.js',
+      exposes: {
+        './SignOut': './src/components/SignOut',
+      },
+      remotes: {
+        'lib-app': 'lib_app@http://localhost:3000/remoteEntry.js',
+      },
     }),
-    // new webpack.HotModuleReplacementPlugin(),
-    new ReactRefreshWebpackPlugin(),
-  ].filter(Boolean),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 })
